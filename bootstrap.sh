c560253d15bf943074f8d9e00e289a4ad7288a62
Davide Gessa <gessadavide@gmail.com>
2019-04-02 20:21:31 +0200

    vagrant final setup: it fails on conf-hidapi as expected

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,2 @@
 #portsnapfetch extract update
-pkg install -y ocaml automake git ocaml-opam
+pkg install -y ocaml automake git ocaml-opam hidapi gmp gmake pkgconf libev bash
--- tezos.sh
+++ tezos.sh
@@ -4,7 +4,9 @@ opam upgrade -y
 opam switch create 4.06.1 -y
 opam switch set 4.06.1 -y
 eval `opam env`
-git clone https://gitlab.com/tezos/tezos 
+rm -rf tezos
+git clone https://gitlab.com/tezos/tezos.git/
 cd tezos 
-make build_deps 
-make
+gmake build-deps 
+gmake
+./tezos-node run
